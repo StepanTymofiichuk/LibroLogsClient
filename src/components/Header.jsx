f5f4686b2bd8778ec8306019e33ad2e7414d584e
Stepan <tymofiichyk1995@gmail.com>
2023-10-16 19:05:58 +0300

    update imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../slices/AuthSlice';
+import { logout } from '../slices/authSliceuthSlice';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
